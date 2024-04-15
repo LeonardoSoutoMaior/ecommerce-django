efb9119b178ab3c1e96b1c6606768225821f69e1
Leonardo Souto <leo.soutomaior@hotmail.com>
2024-04-15 16:47:36 -0300

    6° commit

--- store/models.py
+++ store/models.py
@@ -30,13 +30,23 @@ class Product(models.Model):
     
     
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    date_orderd = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True) 
+    date_ordered = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=200, null=True)
     
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+        
     
     @property
     def get_cart_total(self):
